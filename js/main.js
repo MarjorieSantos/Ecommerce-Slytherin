47c25da985fcc8933027e0b9c00df501dd705639
Marjorie Santos Coutinho Ferreira <marjorie.2001@hotmail.com>
2020-11-16 19:16:59 -0300

    chamando todos os produtos no main

--- js/main.js
+++ js/main.js
@@ -1,23 +1,3 @@
-import produtos from './products.js';
+import printItens from "./printproducts.js";
 
-const principalContent = document.querySelector(".js_itens");
-
-const printItens = () => {
-  produtos.map(item => {
-    const itens = document.createElement("div")
-    itens.classList.add("itens")
-    itens.innerHTML = `
-        <div class="itens-shop">
-          <img class="img-item" src=${item.img}>
-          <div>
-            <h1>${item.name}</h1>
-            <p>R$ ${item.price}</p>
-          </div>
-        </div>
-      `
-
-    principalContent.append(itens)
-  })
-}
-
-printItens()
+printItens();
---
+++ js/printproducts.js
@@ -0,0 +1,22 @@
+import produtos from './products.js';
+
+const principalContent = document.querySelector(".js_itens");
+
+export default function printItens() {
+  produtos.map(item => {
+    const itens = document.createElement("div")
+    itens.classList.add("itens")
+    itens.innerHTML = `
+        <div class="itens-shop">
+          <img class="img-item" src=${item.img}>
+          <div>
+            <h1>${item.name}</h1>
+            <p>R$ ${item.price}</p>
+          </div>
+        </div>
+      `
+
+    principalContent.append(itens)
+  })
+}
+
